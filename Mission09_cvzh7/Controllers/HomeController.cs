b1192f6afecf02e39d40460a6c1d3866b59074fa
CZandt <cole0hardy@gmail.com>
2023-03-01 15:46:35 -0700

    Add project files.

---
+++ Mission09_cvzh7/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_cvzh7.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_cvzh7.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+    }
+}
